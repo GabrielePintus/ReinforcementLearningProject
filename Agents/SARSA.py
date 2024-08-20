f1d8a6ae29c84ef2204befbe429fecc9fafed3e4
NicolaCortinovis <nicolacortinovis98@gmail.com>
2024-08-20 12:09:40 +0200

    updated

--- Agents/SARSA.py
+++
@@ -1,6 +0,0 @@
-from Agents.Agent import Agent
-
-class SARSAAgent(Agent):
-    def __init__(self, num_actions, epsilon=0.1, learning_rate=0.1, discount_factor=0.99):
-        (super(SARSAAgent, self).__init__(epsilon))
-        pass
