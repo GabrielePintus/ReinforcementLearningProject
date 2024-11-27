32d3824829e871068e25815437421fa1a58b982d
Gabriele Pintus <gabrielegpintus@gmail.com>
2024-11-27 14:28:32 +0100

    New approximator

--- NUOVO/approximators.py
+++ NUOVO/approximators.py
@@ -1,162 +1,162 @@
-# import torch
-# import torch.nn as nn
-# import torch.nn.functional as F
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 import numpy as np
 
 
 
-# class MLP(nn.Module):
+class MLP(nn.Module):
 
-#     def __init__(
-#         self,
-#         input_size,
-#         output_size,
-#         hidden_sizes,
-#         activation=nn.ReLU,
-#         dropout=0.0,
-#         device=torch.device('cpu')
-#     ):
-#         super(MLP, self).__init__()
-#         self.input_size = input_size
-#         self.output_size = output_size
-#         self.hidden_sizes = hidden_sizes
-#         self.activation = activation()
-#         self.softmax = nn.Softmax(dim=1)
-#         self.dropout = dropout
-#         self.device = device
-#         self._build_network()
-#         self.init_weights()
+    def __init__(
+        self,
+        input_size,
+        output_size,
+        hidden_sizes,
+        activation=nn.ReLU,
+        dropout=0.0,
+        device=torch.device('cpu')
+    ):
+        super(MLP, self).__init__()
+        self.input_size = input_size
+        self.output_size = output_size
+        self.hidden_sizes = hidden_sizes
+        self.activation = activation()
+        self.softmax = nn.Softmax(dim=1)
+        self.dropout = dropout
+        self.device = device
+        self._build_network()
+        self.init_weights()
 
-#     def _build_network(self):
-#         self.layers = nn.ModuleList()
-#         # Add batch normalization to the input layer
-#         self.layers.append(nn.Linear(self.input_size, self.hidden_sizes[0]))
-#         self.layers.append(self.activation)
-#         for i in range(1, len(self.hidden_sizes)):
-#             self.layers.append(nn.Dropout(self.dropout))
-#             self.layers.append(nn.Linear(self.hidden_sizes[i-1], self.hidden_sizes[i]))
-#             self.layers.append(self.activation)
-#         self.layers.append(nn.Dropout(self.dropout))
-#         self.layers.append(nn.Linear(self.hidden_sizes[-1], self.output_size))
+    def _build_network(self):
+        self.layers = nn.ModuleList()
+        # Add batch normalization to the input layer
+        self.layers.append(nn.Linear(self.input_size, self.hidden_sizes[0]))
+        self.layers.append(self.activation)
+        for i in range(1, len(self.hidden_sizes)):
+            self.layers.append(nn.Dropout(self.dropout))
+            self.layers.append(nn.Linear(self.hidden_sizes[i-1], self.hidden_sizes[i]))
+            self.layers.append(self.activation)
+        self.layers.append(nn.Dropout(self.dropout))
+        self.layers.append(nn.Linear(self.hidden_sizes[-1], self.output_size))
 
-#     def forward(self, x):
-#         for layer in self.layers:
-#             x = layer(x)
-#         return self.softmax(x)
+    def forward(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return self.softmax(x)
     
-#     def init_weights(self):
-#         for layer in self.layers:
-#             if isinstance(layer, nn.Linear):
-#                 nn.init.xavier_normal_(layer.weight)
-#                 nn.init.zeros_(layer.bias)
+    def init_weights(self):
+        for layer in self.layers:
+            if isinstance(layer, nn.Linear):
+                nn.init.xavier_normal_(layer.weight)
+                nn.init.zeros_(layer.bias)
         
 
-# class QValueApproximator:
+class QValueApproximator:
 
-#     def __init__(self, model, learning_rate=1e-4, std_values=np.array):
-#         self.model = model
-#         self.optimizer = torch.optim.SGD(self.model.parameters(), lr=learning_rate, weight_decay=1e-3)#, betas=(0.9, 0.999))
-#         self.criterion = nn.MSELoss()  # Mean Squared Error loss for regression
-#         self.data_loader = None
-#         self.n_epochs = 3
-#         self.batch_size = 40
-#         self.buffer = []
-#         self.loss = np.inf
-#         self.std_values = torch.tensor(std_values, dtype=torch.float32).to(self.model.device)
+    def __init__(self, model, learning_rate=1e-4, std_values=np.array):
+        self.model = model
+        self.optimizer = torch.optim.SGD(self.model.parameters(), lr=learning_rate, weight_decay=1e-3)#, betas=(0.9, 0.999))
+        self.criterion = nn.MSELoss()  # Mean Squared Error loss for regression
+        self.data_loader = None
+        self.n_epochs = 3
+        self.batch_size = 40
+        self.buffer = []
+        self.loss = np.inf
+        self.std_values = torch.tensor(std_values, dtype=torch.float32).to(self.model.device)
 
-#     def predict(self, state):
-#         """
-#         Predict Q-values for a given state using the model.
+    def predict(self, state):
+        """
+        Predict Q-values for a given state using the model.
 
-#         Parameters:
-#         - state (np.ndarray): The input state to predict Q-values for.
+        Parameters:
+        - state (np.ndarray): The input state to predict Q-values for.
 
-#         Returns:
-#         - q_values (torch.Tensor): Predicted Q-values for all actions.
-#         """
-#         # Convert state to tensor and move to the appropriate device
-#         if not torch.is_tensor(state):
-#             state = torch.tensor(state).float().to(self.model.device)
-#         with torch.no_grad():  # No need to track gradients for prediction
-#             # print("State before", state)
-#             state = (state - self.std_values[:, 0]) / self.std_values[:, 1]
-#             # print("State after", state)
-#             q_values = self.model(state.unsqueeze(0))  # Add batch dimension
-#         return q_values.squeeze(0)  # Remove batch dimension
+        Returns:
+        - q_values (torch.Tensor): Predicted Q-values for all actions.
+        """
+        # Convert state to tensor and move to the appropriate device
+        if not torch.is_tensor(state):
+            state = torch.tensor(state).float().to(self.model.device)
+        with torch.no_grad():  # No need to track gradients for prediction
+            # print("State before", state)
+            state = (state - self.std_values[:, 0]) / self.std_values[:, 1]
+            # print("State after", state)
+            q_values = self.model(state.unsqueeze(0))  # Add batch dimension
+        return q_values.squeeze(0)  # Remove batch dimension
 
-#     def update(self, y_true, y_pred):
-#         """
-#         Update the model using the provided target and predicted Q-values.
+    def update(self, y_true, y_pred):
+        """
+        Update the model using the provided target and predicted Q-values.
 
-#         Parameters:
-#         - y_true (torch.Tensor): Target Q-values.
-#         - y_pred (torch.Tensor): Predicted Q-values.
+        Parameters:
+        - y_true (torch.Tensor): Target Q-values.
+        - y_pred (torch.Tensor): Predicted Q-values.
 
-#         Returns:
-#         - loss (float): The loss value.
-#         """
-#         # Check if input is tensor or numpy array
-#         if not torch.is_tensor(y_true):
-#             y_true = torch.tensor(y_true, requires_grad=True, dtype=torch.float32).to(self.model.device)
-#         if not torch.is_tensor(y_pred):
-#             y_pred = torch.tensor(y_pred, requires_grad=True, dtype=torch.float32).to(self.model.device)
+        Returns:
+        - loss (float): The loss value.
+        """
+        # Check if input is tensor or numpy array
+        if not torch.is_tensor(y_true):
+            y_true = torch.tensor(y_true, requires_grad=True, dtype=torch.float32).to(self.model.device)
+        if not torch.is_tensor(y_pred):
+            y_pred = torch.tensor(y_pred, requires_grad=True, dtype=torch.float32).to(self.model.device)
         
-#         # Add to buffer
-#         self.buffer.append((y_pred, y_true))
+        # Add to buffer
+        self.buffer.append((y_pred, y_true))
 
-#         # Update model if buffer is full
-#         if len(self.buffer) == self.batch_size:
-#             self.data_loader = torch.utils.data.DataLoader(self.buffer, batch_size=self.batch_size, shuffle=True)
-#             self.buffer = []
+        # Update model if buffer is full
+        if len(self.buffer) == self.batch_size:
+            self.data_loader = torch.utils.data.DataLoader(self.buffer, batch_size=self.batch_size, shuffle=True)
+            self.buffer = []
         
-#             # Train the model
-#             for epoch in range(self.n_epochs):
-#                 for y_pred, y_true in self.data_loader:
-#                     # Ensure data is on the correct device
-#                     y_pred = y_pred.to(self.model.device)
-#                     y_true = y_true.to(self.model.device)
+            # Train the model
+            for epoch in range(self.n_epochs):
+                for y_pred, y_true in self.data_loader:
+                    # Ensure data is on the correct device
+                    y_pred = y_pred.to(self.model.device)
+                    y_true = y_true.to(self.model.device)
 
-#                     # Zero the gradients
-#                     self.optimizer.zero_grad()
+                    # Zero the gradients
+                    self.optimizer.zero_grad()
 
-#                     # Forward pass
-#                     loss = self.criterion(y_pred, y_true)
+                    # Forward pass
+                    loss = self.criterion(y_pred, y_true)
 
-#                     # Backward pass
-#                     loss.backward()
+                    # Backward pass
+                    loss.backward()
 
-#                     # Gradient clipping
-#                     torch.nn.utils.clip_grad_norm_(self.model.parameters(), 0.5)
+                    # Gradient clipping
+                    torch.nn.utils.clip_grad_norm_(self.model.parameters(), 0.5)
 
-#                     # Update weights
-#                     self.optimizer.step()
+                    # Update weights
+                    self.optimizer.step()
             
-#             self.loss = loss.item()
+            self.loss = loss.item()
 
 
 
-#         return self.loss
+        return self.loss
 
 
 
 
 
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, Ridge
 from sklearn.kernel_ridge import KernelRidge
+# Random forest regressor
+from sklearn.ensemble import RandomForestRegressor
+from xgboost import XGBRegressor
 
-class TilingApproximator:
+
+class TilingApproximatorSmall:
 
     def __init__(self, bounds, n_tiles, n_tilings, shifts):
         self.bounds = bounds
         self.n_tiles = n_tiles
         self.n_tilings = n_tilings
-        self.shifts = shifts
+        self.shifts = shifts        
+        self.model = KernelRidge(kernel='poly', degree=2, gamma=0.5)
         
-        self.model = KernelRidge(kernel='rbf', gamma=0.5)
-        self.model = KernelRidge(kernel='poly', degree=3)
-        # self.model = LinearRegression()
-        
-
     def rescale(self, state):
         return (state - self.bounds[:, 0]) / (self.bounds[:, 1] - self.bounds[:, 0])
 
@@ -180,17 +180,47 @@ class TilingApproximator:
         self.model.fit(Z, y)
 
 
+class TilingApproximatorMedium:
+
+    def __init__(self, bounds, n_tiles, n_tilings, shifts):
+        self.bounds = bounds
+        self.n_tiles = n_tiles
+        self.n_tilings = n_tilings
+        self.shifts = shifts
+        self.model = LinearRegression()
+
+    def rescale(self, state):
+        return (state - self.bounds[:, 0]) / (self.bounds[:, 1] - self.bounds[:, 0])
+
+    def tile_encode(self, state):
+        z = np.zeros((self.n_tilings, len(state)))
+        state = self.rescale(state)
+
+        for tiling in range(self.n_tilings):
+            s = (state + self.shifts*tiling) % 1
+            s = (s * self.n_tiles).astype(int)
+            z[tiling,:] = s
+
+        return z
+    
+    def fit(self, X, y):
+        Z = np.array([self.tile_encode(x) for x in X])
+        Z = Z.reshape(Z.shape[0], -1)
+        self.model.fit(Z, y)
+
+    def predict(self, state):
+        z = self.tile_encode(state)
+        z = z.reshape(1, -1)
+        return self.model.predict(z)[0]
 
 
 
-
+from sklearn.metrics import mean_squared_error
 
 if __name__ == "__main__":
     
-    # f = lambda x: np.sum(x) + np.random.normal(0, 0.5)
-    # f = lambda x: np.sin(x[0]) + np.cos(x[1]) + np.random.normal(0, 0.1)
-    f = lambda x: np.sin(x[0]) + np.cos(x[1]) * x[0] + np.random.normal(0, 0.1)
-    
+
+    f = lambda x: np.random.normal(0, 0.5) + np.sin(x[0]*x[1]) + np.log(x[0] + x[1] + 1) * np.sqrt(x[0] + x[1] + 1)    
     X = np.random.rand(100, 2) * 3
     y = np.array([f(x) for x in X])
 
@@ -199,31 +229,23 @@ if __name__ == "__main__":
     n_tilings = 32
     shifts = np.array([0.1, 0.3])
 
-    tiling = TilingApproximator(bounds, n_tiles, n_tilings, shifts)
-    
+    tiling = TilingApproximatorMedium(bounds, n_tiles, n_tilings, shifts)
+
+    # Fit the model
     tiling.fit(X, y)
 
-    y_pred = np.array([tiling.predict(x) for x in X]).round(2)
+    # Predict the first data point
+    y_preds = [tiling.predict(x) for x in X]
+    train_mse = mean_squared_error(y, y_preds)
+    print("Train MSE:", train_mse)
 
-    for i in range(len(X)):
-        print(f"prediction: {y_pred[i]}, true: {y[i]}")
+    X_test = np.random.rand(100, 2) * 3
+    y_test = np.array([f(x) for x in X_test])
+    y_preds = [tiling.predict(x) for x in X_test]
+    test_mse = mean_squared_error(y_test, y_preds)
+    print("Test MSE:", test_mse)
+    
 
-    mse = np.mean((y - y_pred)**2)
-    print(f"MSE: {mse}")
-
-
-    # Plot 
-    import matplotlib.pyplot as plt
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(X[:, 0], X[:, 1], y, color='r', marker='o', label='True')
-    ax.scatter(X[:, 0], X[:, 1], y_pred, color='b', marker='x', label='Predicted')
-    ax.set_xlabel('X Label')
-    ax.set_ylabel('Y Label')
-    ax.set_zlabel('Z Label')
-
-    plt.show()
 
 
 
