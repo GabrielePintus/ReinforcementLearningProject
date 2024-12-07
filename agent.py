edc339fbb775b1d9c7be11e9b62e6424e99b2622
Gabriele Pinus <gabrielegpintus@gmail.com>
2024-12-07 23:33:35 +0100

    Pulizia

--- agent.py
+++ agent.py
@@ -114,32 +114,23 @@ class QAgent(LearningAgent):
         # Q-value approximator
         self.q_values = np.zeros((env.observation_space.n, env.action_space.n))
 
-    def learn(self, n_episodes=1000):
+    def learn(self, n_episodes=1000, horizon=10_000):
         """
         Learn the optimal policy.
         """
         rewards = []
-        max_steps = 100
         steps = 0
         for _ in tqdm(range(n_episodes)):
             state, _ = self.env.reset()
             done = False
             cum_reward = 0
-            while not done and steps < max_steps:
+            while not done and steps < horizon:
                 # Sample action from the behaviour policy
                 action = self.behaviour_policy(state)
 
                 # Take action and observe reward and next state
                 next_state, reward, done, _, _ = self.env.step(action)
-
-                # Lazyness penalty
-                # if not done:
-                #     reward += -0.1
-
-                # Check if the agent has fallen into a hole
-                if done and reward == 0:
-                    reward = -1
-
+                
                 cum_reward += reward
 
                 # Compute the best action for the next state
@@ -157,7 +148,6 @@ class QAgent(LearningAgent):
 
         return rewards
     
-
 
 
 
@@ -182,7 +172,7 @@ class QLambdaAgent(LearningAgent):
         self.eligibility_trace = np.zeros((env.observation_space.n, env.action_space.n))
 
 
-    def learn(self, n_episodes=1000):
+    def learn(self, n_episodes=1000, horizon=10_000):
         """
         Learn the optimal policy.
         """
@@ -192,21 +182,12 @@ class QLambdaAgent(LearningAgent):
             action = self.behaviour_policy(state)
             done = False
             cum_reward = 0
-            max_steps = 100
             steps = 0
 
-            while not done and steps < max_steps:
+            while not done and steps < horizon:
                 # Take action and observe reward and next state
                 next_state, reward, done, _, _ = self.env.step(action)
-
-                # Lazyness penalty
-                # if not done:
-                #     reward += -0.1
-
-                # Check if the agent has fallen into a hole
-                if done and reward == 0:
-                    reward = -1
-
+                
                 # Compute the best action for the next state
                 cum_reward += reward
 
@@ -220,87 +201,22 @@ class QLambdaAgent(LearningAgent):
                 target = reward + (self.discount_factor * self.q_values[next_state, next_best_action])
                 delta = target - self.q_values[state, action]
 
-                # Update Eligibility Trace and Q-values
+                # Update Eligibility Trace and normalize it
                 self.eligibility_trace[state, action] += 1
-                # self.eligibility_trace[state, action] = max(self.eligibility_trace[state, action], 1)
-
-                for s, a in np.ndindex(self.q_values.shape):
-                    self.q_values[s, a] += self.learning_rate * delta * self.eligibility_trace[s, a]
-                    if next_action == next_best_action:
-                        self.eligibility_trace[s, a] *= self.discount_factor * self.trace_decay
-                    else:
-                        self.eligibility_trace[s, a] = 0
-                    # Reset Eligibility Trace if it is too small
-                    if self.eligibility_trace[s, a] < 1e-3:
-                        self.eligibility_trace[s, a] = 0
-
-
-                state = next_state
-                action = next_action
-                steps += 1
-
-            self.update_epsilon()
-            rewards.append(cum_reward)
-
-        return rewards
-
-
-
-
-class SarsaAgent(LearningAgent):
-
-    def __init__(
-            self,
-            env: gym.Env,
-            discount_factor=0.99,
-            initial_epsilon=0.5,
-            epsilon_decay=0.97,
-            min_epsilon=0.0,
-            learning_rate=0.9,
-            seed=0,
-        ):
-        super().__init__(env, discount_factor, initial_epsilon, epsilon_decay, min_epsilon, learning_rate, seed)
-
-        # Q-value approximator
-        self.q_values = np.zeros((env.observation_space.n, env.action_space.n))
-
-    def learn(self, n_episodes=1000):
-        """
-        Learn the optimal policy.
-        """
-        rewards = []
-        for episode in tqdm(range(n_episodes)):
-            state, _ = self.env.reset()
-            action = self.behaviour_policy(state)
-            done = False
-            cum_reward = 0
-            max_steps = np.inf
-            steps = 0
-
-            while not done and steps < max_steps:
-                # Take action and observe reward and next state
-                next_state, reward, done, _, _ = self.env.step(action)
-
-                # Lazyness penalty
-                # if not done:
-                #     reward += -0.1
-
-                # Check if the agent has fallen into a hole
-                if done and reward == 0:
-                    reward = -1
-
-                # Compute the best action for the next state
-                cum_reward += reward
-
-                # Take action from Behaviour Policy
-                next_action = self.behaviour_policy(next_state)
-
-                # Compute the TD error
-                target = reward + (self.discount_factor * self.q_values[next_state, next_action])
-                delta = target - self.q_values[state, action]
+                et_min = np.min(self.eligibility_trace)
+                et_max = np.max(self.eligibility_trace)
+                if et_max - et_min > 1e-3:
+                    self.eligibility_trace -= et_min
+                    self.eligibility_trace /= (et_max - et_min)
 
                 # Update Q-values
-                self.q_values[state, action] += self.learning_rate * delta
+                self.q_values += self.learning_rate * delta * self.eligibility_trace
+                
+                # Apply the trace decay where the next action is the best action                
+                self.eligibility_trace *= (next_action == next_best_action) * self.discount_factor * self.trace_decay
+                
+                # Set to zero the trace where < 1e-3
+                # self.eligibility_trace[self.eligibility_trace < 1e-3] = 0
 
                 state = next_state
                 action = next_action
@@ -312,82 +228,6 @@ class SarsaAgent(LearningAgent):
         return rewards
 
 
-class SarsaLambdaAgent(LearningAgent):
-
-    def __init__(
-            self,
-            env: gym.Env,
-            discount_factor=0.99,
-            initial_epsilon=0.5,
-            epsilon_decay=0.97,
-            min_epsilon=0.0,
-            learning_rate=0.9,
-            seed=0,
-            trace_decay=0.9
-        ):
-        super().__init__(env, discount_factor, initial_epsilon, epsilon_decay, min_epsilon, learning_rate, seed)
-        
-        self.trace_decay = trace_decay
-
-        # Q-value approximator
-        self.q_values = np.zeros((env.observation_space.n, env.action_space.n))
-        self.eligibility_trace = np.zeros((env.observation_space.n, env.action_space.n))
-
-    def learn(self, n_episodes=1000):
-        """
-        Learn the optimal policy.
-        """
-        rewards = []
-        for episode in tqdm(range(n_episodes)):
-            state, _ = self.env.reset()
-            action = self.behaviour_policy(state)
-            done = False
-            cum_reward = 0
-            max_steps = 100
-            steps = 0
-
-            while not done and steps < max_steps:
-                # Take action and observe reward and next state
-                next_state, reward, done, _, _ = self.env.step(action)
-
-                # Lazyness penalty
-                # if not done:
-                #     reward += -0.05
-
-                # Check if the agent has fallen into a hole
-                if done and reward == 0:
-                    reward = -1
-
-                # Sum the reward
-                cum_reward += reward
-
-                # Compute the next action according to the behaviour policy
-                next_action = self.behaviour_policy(next_state)
-
-                # Compute the TD error
-                target = reward + (self.discount_factor * self.q_values[next_state, next_action])
-                delta = target - self.q_values[state, action]
-
-                # Update Eligibility Trace and Q-values
-                self.eligibility_trace[state, action] += 1
-                # self.eligibility_trace[state, action] = max(self.eligibility_trace[state, action], 1)
-
-                for s, a in np.ndindex(self.q_values.shape):
-                    self.q_values[s, a] += self.learning_rate * delta * self.eligibility_trace[s, a]
-                    self.eligibility_trace[s, a] *= self.discount_factor * self.trace_decay
-                    # Reset Eligibility Trace if it is too small
-                    if self.eligibility_trace[s, a] < 1e-3:
-                        self.eligibility_trace[s, a] = 0
-
-                state = next_state
-                action = next_action
-                steps += 1
-
-            self.update_epsilon()
-            rewards.append(cum_reward)
-
-        return rewards
-            
 
 
 
@@ -414,7 +254,7 @@ class QSpatialLambdaAgent(LearningAgent):
         self.q_values = np.zeros((env.observation_space.n, env.action_space.n))
         self.eligibility_trace = np.zeros((env.observation_space.n, env.action_space.n))
 
-    def learn(self, n_episodes=1000):
+    def learn(self, n_episodes=1000, horizon=10_000):
         """
         Learn the optimal policy.
         """
@@ -424,21 +264,12 @@ class QSpatialLambdaAgent(LearningAgent):
             action = self.behaviour_policy(state)
             done = False
             cum_reward = 0
-            max_steps = 100
             steps = 0
 
-            while not done and steps < max_steps:
+            while not done and steps < horizon:
                 # Take action and observe reward and next state
                 next_state, reward, done, _, _ = self.env.step(action)
-
-                # Lazyness penalty
-                # if not done:
-                #     reward += -0.1
-
-                # Check if the agent has fallen into a hole
-                if done and reward == 0:
-                    reward = -1
-
+                    
                 # Compute the best action for the next state
                 cum_reward += reward
 
@@ -452,22 +283,38 @@ class QSpatialLambdaAgent(LearningAgent):
                 target = reward + (self.discount_factor * self.q_values[next_state, next_best_action])
                 delta = target - self.q_values[state, action]
 
-                # Update Eligibility Trace and Q-values
+                # Update Eligibility Trace and Normalize it
                 self.eligibility_trace[state, action] += 1
-                # self.eligibility_trace[state, action] = max(self.eligibility_trace[state, action], 1)
+                et_min = np.min(self.eligibility_trace)
+                et_max = np.max(self.eligibility_trace)
+                if et_max - et_min > 1e-3:
+                    self.eligibility_trace -= et_min
+                    self.eligibility_trace /= (et_max - et_min)
+                    
+                # Update Q-values
+                self.q_values += self.learning_rate * delta * self.eligibility_trace
+  
+                # Apply the trace decay where the next action is the best action
+                mask = (next_action == next_best_action)
+                self.eligibility_trace *= mask * self.discount_factor * self.trace_decay
+                
+                # Apply kernel spatial decay where the next action is not the best action
+                if next_action != next_best_action:
+                    # Create a grid of state-action pairs
+                    state_action_pairs = np.array(np.meshgrid(np.arange(self.q_values.shape[0]), np.arange(self.q_values.shape[1]))).T.reshape(-1, 2)
+                    next_state_action = np.array([next_state, next_action])
+                    
+                    # Compute the kernel values for all state-action pairs
+                    kernel_values = np.apply_along_axis(lambda pair: self.kernel(pair, next_state_action), 1, state_action_pairs)
+                    
+                    # Reshape kernel values to match the shape of eligibility_trace
+                    kernel_values = kernel_values.reshape(self.q_values.shape)
+                    
+                    # Apply the kernel values to decay the eligibility trace
+                    self.eligibility_trace *= kernel_values
 
-                for s, a in np.ndindex(self.q_values.shape):
-                    self.q_values[s, a] += self.learning_rate * delta * self.eligibility_trace[s, a]
-                    if next_action == next_best_action:
-                        self.eligibility_trace[s, a] *= self.discount_factor * self.trace_decay
-                    else:
-                        # Decay the trace using gaussian kernel
-                        value = self.kernel(np.array([s, a]), np.array([next_state, next_action]))
-                        self.eligibility_trace[s, a] *= value
-                    # Reset Eligibility Trace if it is too small
-                    if self.eligibility_trace[s, a] < 1e-3:
-                        self.eligibility_trace[s, a] = 0
-
+                # Set to zero the trace where < 1e-3
+                # self.eligibility_trace[self.eligibility_trace < 1e-3] = 0
 
                 state = next_state
                 action = next_action
