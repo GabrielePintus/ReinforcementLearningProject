ac8cdd0cd5a8a0e946800e4aa2e0940175bcc304
Gabriele Pinus <gabrielegpintus@gmail.com>
2025-02-05 22:33:51 +0100

    parallel ppo

--- src/DeepAgents.py
+++ src/DeepAgents.py
@@ -288,6 +288,7 @@ class MonteCarloPolicyGradient(LearningAgent):
     def learn(self, n_episodes=1000, horizon=10_000):
         rewards = []
         losses = []
+        total_rewards = []
         
         progress_bar = tqdm(range(n_episodes), desc='Simulating')
         for _ in progress_bar:
@@ -639,5 +640,188 @@ class ProximalPolicyOptimization(LearningAgent):
     
     
 
+class MiniBatchProximalPolicyOptimization(LearningAgent):
+    
+    def __init__(
+        self,
+        env : gym.Env,
+        discount_factor=0.99,
+        policy_net : torch.nn.Module = None,
+        value_net : torch.nn.Module = None,
+        ratio_clip=0.2,
+        entropy_weight=0.0,
+        batch_size=32,
+        n_epochs=2
+    ):
+        self.env = env
+        self.discount_factor = discount_factor
+        
+        # Policy network
+        self.policy_net = policy_net
+        self.value_net = value_net
+        # Initialize weights
+        self.policy_net.init_weights(True)
+        self.value_net.init_weights(True)
+        # Old policy network
+        self.policy_net_old = deepcopy(policy_net)
+                
+        # Loss params
+        self.ratio_clip = ratio_clip
+        self.entropy_weight = entropy_weight
+        
+        # Mini-batch params
+        self.batch_size = batch_size
+        self.n_epochs = n_epochs
+        
+        
+    def policy(self, state):
+        if not isinstance(state, torch.Tensor):
+            state = torch.tensor(state, dtype=torch.float32)
+        state = state.to(self.policy_net_old.device)
+        
+        self.policy_net_old.eval()
+        with torch.no_grad():
+            action_probs = self.policy_net_old(state)
+        action = Categorical(action_probs).sample().cpu().numpy()
+            
+        return action
+    
+    def critic(self, state):
+        if not isinstance(state, torch.Tensor):
+            state = torch.tensor(state, dtype=torch.float32)
+        state = state.to(self.value_net.device)
+        
+        self.value_net.eval()
+        with torch.no_grad():
+            value = self.value_net(state)
+            
+        return value
+    
+    
+    def sample_trajectory(self, horizon=10_000):
+        state, _ = self.env.reset()
+        
+        done = [ False ] * state.shape[0]
+        truncated = [ False ] * state.shape[0]
+        episode = []
+        
+        steps = 0
+        # while not done and not truncated and steps < horizon:
+        while not any(done) and not any(truncated) and steps < horizon:
+            action = self.policy(state)
+            next_state, reward, done, truncated, _ = self.env.step(action)
+            episode.append((state, action, reward))
+            state = next_state
+            steps += 1
+            
+        return episode
+    
+    
+    def learn(self, n_episodes=1000, horizon=10_000):
+        rewards, losses_actor, losses_critic = [], [], []
+        
+        progress_bar = tqdm(range(n_episodes), desc='Simulating')
+        for _ in progress_bar:
+            trajectory = self.sample_trajectory(horizon)
+            
+            # Compute returns and advantages
+            G = 0
+            returns = []
+            values = []
+            for state, _, reward in trajectory[::-1]:
+                # Compute the return
+                G = reward + self.discount_factor * G
+                returns.append(G)
+                
+                # Compute the value estimate of the state
+                value = self.critic(state)
+                values.append(value)   
+            
+            returns = np.array([ r for r in returns[::-1]])
+            values = np.array([ v.cpu().numpy() for v in values[::-1]])
+            
+            # Compute the advantages
+            returns = torch.tensor(returns, dtype=torch.float32).to(self.value_net.device)
+            values = torch.tensor(values, dtype=torch.float32).to(self.value_net.device).view(returns.shape)
+            
+            # Compute the advantages
+            advantages = (returns - values).detach()
+            
+            # Move the states, actions and advantages to the device
+            states, actions, _ = zip(*trajectory)
+            states = torch.tensor(states, dtype=torch.float32).to(self.policy_net.device)
+            actions = torch.tensor(actions, dtype=torch.long).to(self.policy_net.device)
+            
+            # Flatten the multiple trajectories
+            T, N, *obs_shape = states.shape
+            states = states.view(T * N, *obs_shape)     # Now shape: (T*N, obs_dim)
+            actions = actions.view(T * N)               # Now shape: (T*N,)
+            advantages = advantages.view(T * N)         # Now shape: (T*N,)
+            returns = returns.view(T * N)               # Now shape: (T*N,)
+            
+            # Create the mini batch data loaders
+            actor_dataset = torch.utils.data.TensorDataset(states, actions, advantages)
+            critic_dataset = torch.utils.data.TensorDataset(states, returns)
+            actor_loader = torch.utils.data.DataLoader(actor_dataset, batch_size=self.batch_size, shuffle=True)
+            critic_loader = torch.utils.data.DataLoader(critic_dataset, batch_size=self.batch_size, shuffle=True)            
+            
+            for _ in range(self.n_epochs):
+                for states, actions, advantages in actor_loader:
+                    # Compute the probability ratio with the old policy
+                    action_probs_old = self.policy_net_old(states)
+                    action_log_probs_old = Categorical(action_probs_old).log_prob(actions)
+
+                    # Compute the probability ratio with the current policy
+                    action_probs = self.policy_net(states)
+                    action_log_probs = Categorical(action_probs).log_prob(actions)
+
+                    # Compute the probability ratio
+                    ratio = torch.exp(action_log_probs - action_log_probs_old)
+
+                    # Compute the surrogate clipped loss
+                    surrogate1 = ratio * advantages
+                    surrogate2 = torch.clamp(ratio, 1 - self.ratio_clip, 1 + self.ratio_clip) * advantages
+
+                    # Compute the entropy loss
+                    entropy = Categorical(action_probs).entropy().mean()
+
+                    # Compute full loss
+                    loss_actor = -torch.min(surrogate1, surrogate2).mean() - self.entropy_weight * entropy
+                    losses_actor.append(loss_actor.item())
+
+                    # Save the current weights for later
+                    policy_nn_state = self.policy_net.state_dict().copy()
+
+                    # Neural network optimization
+                    self.policy_net.train()
+                    self.policy_net.optimizer.zero_grad()
+                    loss_actor.backward()
+                    torch.nn.utils.clip_grad_norm_(self.policy_net.parameters(), 0.5)
+                    self.policy_net.optimizer.step()
+
+                    # Update the old policy network
+                    self.policy_net_old.load_state_dict(policy_nn_state)
+                    
+                for states, returns in critic_loader:
+                    # Train the value network
+                    self.value_net.train()
+                    self.value_net.optimizer.zero_grad()
+                    value = self.value_net(states)
+                    loss_critic = F.huber_loss(value, returns.view(-1, 1))
+                    loss_critic.backward()
+                    torch.nn.utils.clip_grad_norm_(self.value_net.parameters(), 0.5)
+                    self.value_net.optimizer.step()
+                    losses_critic.append(loss_critic.item())
+                    
+            # Track the rewards
+            rewards.append(sum(r for _, _, r in trajectory))
+            progress_bar.set_postfix({
+                'reward': np.mean(rewards[-10:]),
+                'loss actor': np.mean(losses_actor[-10:]),
+                'loss critic': np.mean(losses_critic[-10:]),
+            })
+            
+        return rewards, losses_actor, losses_critic
+    
 
 
